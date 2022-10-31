ab98e31a5427a6940a29a2dc26e7161bffb858c1
kevinokello <okellokevin77@gmail.com>
2022-10-31 17:10:30 +0300

    crisp

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,22 +7,7 @@ import { initGA, logPageView } from 'analytics';
 import 'rc-drawer/assets/index.css';
 import 'assets/css/react-slick.css';
 // import "rc-tabs/assets/index.css";
-<Script
-  id="crisp-widget"
-  strategy="afterInteractive"
-  dangerouslySetInnerHTML={{
-    __html: `
-      window.$crisp=[];
-      window.CRISP_WEBSITE_ID="0a0fe994-0166-4acd-ba01-f78afa76ee79";
-      (function(){
-        const d = document;
-        const s = d.createElement("script");
-        s.src = "https://client.crisp.chat/l.js";
-        s.async = 1;
-        d.getElementsByTagName("head")[0].appendChild(s);
-      })();`,
-  }}
-/>;
+
 export default function CustomApp({ Component, pageProps }) {
   /** 
    useEffect(() => {
