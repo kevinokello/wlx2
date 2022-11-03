b809a7cf85dbee0a527d620595b00edbee33f791
kevinokello <okellokevin77@gmail.com>
2022-11-03 17:49:20 +0300

    tawk

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,16 +6,23 @@ import { initGA, logPageView } from 'analytics';
 */
 import 'rc-drawer/assets/index.css';
 import 'assets/css/react-slick.css';
+import Script from "next/script";
 // import "rc-tabs/assets/index.css";
 
 export default function CustomApp({ Component, pageProps }) {
-  /** 
-   useEffect(() => {
-     initGA();
-     logPageView();
-     Router.events.on('routeChangeComplete', logPageView);
-   }, []);
-   */
+     <Script id="tawk" strategy="lazyOnload">
+       {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/6363d4a1b0d6371309cd1f7e/1gguvd5rn';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+        `}
+     </Script>;
 
   return <Component {...pageProps} />;
   
